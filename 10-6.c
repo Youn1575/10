b60ec41ab5b5d942f94294b4b1df63b85691abb5
youn1575 <youn461515@gmail.com>
2023-11-09 16:58:39 +0900

    10-6

---
+++ 10-6.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+	FILE* fp;
+	char c;
+	
+	fp = fopen("sample.txt", "r");
+	if (fp == NULL) 
+	{
+		printf("failed to open\n");
+		return 0;
+	}
+		
+	while( (c=fgetc(fp)) != EOF) // 한글자 읽은게 마지막이 아니면 
+	{ 
+		putchar(c);	// 한글자 출력
+	}
+
+	fclose(fp); 
+
+	system("PAUSE");
+	return 0;
+}
+
