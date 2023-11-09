9625f355d91e88bca710724e8a0f86e9350dd3b8
youn1575 <youn461515@gmail.com>
2023-11-09 15:38:40 +0900

    10-4

---
+++ 10-4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+
+	char src[30] = "happy C programming";
+	
+	printf("문자열 \"%s\" 의 길이 : %i\n", src, strlen(src) );	
+	
+	system("PAUSE");
+	return 0;
+}
+
