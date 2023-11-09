bfef9517cf6774ca57e54d46af4ea718ee4545bc
youn1575 <youn461515@gmail.com>
2023-11-09 15:50:09 +0900

    10-5

---
+++ 10-5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+ 	FILE *fp;
+ 	char str[100];
+ 	int i;
+ 	//1. open file
+ 	fp = fopen("sample.txt", "w");
+ 	
+ 	//2. write file
+ 	for (i=0; i<3; i++){
+ 		//2-1. print "input a word"
+		printf("input a word");
+ 		//2-2. scanf a string
+		scanf("%s", str);
+ 		//2-3. fprintf()
+ 		fprintf(fp, "%s\n", str);
+	 }
+ 	//3. close file
+ 	fclose(fp);
+	system("PAUSE");
+	return 0;
+}
+
