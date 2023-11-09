146f9689cb81c820e0e6852ba41b29579cdd1acd
youn1575 <youn461515@gmail.com>
2023-11-09 15:32:52 +0900

    10-3

---
+++ 10-3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+	char src[]="The worst things to eat before you sleep";
+	char dst[100];
+	
+	strcpy(dst, src);
+	
+	printf("copied string : %s\n", dst);	
+	
+	system("PAUSE");
+	return 0;
+}
+
